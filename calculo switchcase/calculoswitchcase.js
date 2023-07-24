e63088c4ac1a1bac291880f5a5846e8be1185461
João Paulo <105462675+joaodias84@users.noreply.github.com>
2023-07-24 13:14:51 -0300

    calculadora SwitchCase
    calculadora simples ultilizando comando SwitchCase javascript

---
+++ calculo switchcase/calculoswitchcase.js
@@ -0,0 +1,29 @@
+function calculadoraSwitchCase(a, b, operacao) {
+  var resultado;
+
+  switch (operacao) {
+    case '+':
+      resultado = a + b;
+      break;
+    case '-':
+      resultado = a - b;
+      break;
+    case '*':flu
+      resultado = a * b;
+      break;
+    case '/':
+      resultado = a / b;
+      break;
+    default:
+      return "Operação inválida. Por favor, insira um símbolo válido (+, -, *, ou /).";
+  }
+
+  return "Resultado: " + resultado;
+}
+
+var num1 = parseFloat(prompt("Digite o primeiro número:"));
+var num2 = parseFloat(prompt("Digite o segundo número:"));
+var operacao = prompt("Digite o símbolo da operação desejada (+, -, *, ou /):");
+
+var resultadoSwitchCase = calculadoraSwitchCase(num1, num2, operacao);
+console.log(resultadoSwitchCase);
